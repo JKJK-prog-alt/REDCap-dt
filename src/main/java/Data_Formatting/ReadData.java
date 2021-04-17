68543bb9c48ebf7c2b5f4e5c69b38b56e3e49f5c
Nick <Noehme@mail.hs-ulm.de>
2021-04-17 17:20:00 +0200

    Data Splitting-Test
    Trying to split Data out of an Arraylist //Autor Lisa and Nick

--- src/main/java/Data_Formatting/ReadData.java
+++ src/main/java/Data_Formatting/ReadData.java
@@ -1,10 +1,32 @@
 package Data_Formatting;
+import java.util.*;
+import java.io.IOException;
+import java.nio.*;
+import java.nio.file.*;
 
 public class ReadData {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	
+	public void split() throws IOException {
+		
+		
+		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Kalli\\Desktop\\DATA_2021-03-31_2145.csv"));
+		List<String> recList = lines;
+		String[] result = new String[100];
+		
+		for(String liste : recList) {
+			result = liste.split(",");			
+					
+		}
+		for (int i = 0; i <= 100; i++) {
+			System.out.println(result[i]);
+		}
 	}
-
+	public static void main(String[] args) throws IOException {
+		
+		ReadData test = new ReadData();
+		test.split();
+		
+	}
 }
