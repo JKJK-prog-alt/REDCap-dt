e36487115bc733857183e696851dca8a6959723f
Lisamaria <leble@mail.hs-ulm.de>
2021-04-21 16:22:06 +0200

    Wrong commit
    Code cleaning again

--- src/main/java/Data_Formatting/ReadData.java
+++ src/main/java/Data_Formatting/ReadData.java
@@ -7,12 +7,9 @@ import java.nio.file.*;
 
 public class ReadData {
 	
-	
-	
 	public void split() throws IOException {
 		
 		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\lisam\\Documents\\Studium\\Projektarbeit\\Gruppe1\\Daten\\DATA_2021-03-31_2145.csv"));
-		String[] result = new String[100];
 		String liste="";
 		StringBuffer sb = new StringBuffer(liste);	
 		
@@ -20,11 +17,7 @@ public class ReadData {
 			sb.append(lines.get(i)+ ",");
 		}
 		liste = sb.toString();	
-	
 		List<String> data = new ArrayList(Arrays.asList(liste.split(",")));
-		for (int x=0; x<=19; x++) {
-			System.out.println(result[x]);
-		}
 	}
 	
 	public static void main(String[] args) throws IOException {
