c125e50cb54938cae98b08d5962d10cf3f30261d
Lisamaria <leble@mail.hs-ulm.de>
2021-04-21 16:01:51 +0200

    Data Splitting
    more effective way getting data in an ArrayList from csv without an String Array // Autor: Nick & Lisamaria

--- src/main/java/Data_Formatting/ReadData.java
+++ src/main/java/Data_Formatting/ReadData.java
@@ -1,29 +1,37 @@
 package Data_Formatting;
 import java.util.*;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
 import java.io.IOException;
 import java.nio.*;
 import java.nio.file.*;
 
 public class ReadData {
 	
-	
-	
 	public void split() throws IOException {
 		
 		
 		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\lisam\\Documents\\Studium\\Projektarbeit\\Gruppe1\\Daten\\DATA_2021-03-31_2145.csv"));
-		String[] result = new String[100];
-		String liste="";
 		
-		for(int i=0; i<=3; i++) {
-			StringBuffer sb = new StringBuffer(liste);
-			sb.append(lines.get(i));
+		String liste="";
+		StringBuffer sb = new StringBuffer(liste);
+	//	String[] result = new String[100];
+		
+		
+		System.out.println(lines);
+		for(int i = 0; i < lines.size(); i++) {
 			
-			liste = sb.toString();		
+			sb.append(lines.get(i)+ ",");
 		}
-		result = liste.split(",");	
+		liste = sb.toString();	
+		
+		System.out.println(liste);
+		//result = liste.split(",");
+		List<String> data = new ArrayList(Arrays.asList(liste.split(",")));
 		for (int x=0; x<=19; x++) {
-			System.out.println(result[x]);
+			System.out.println(data.get(x));
 		}
 		
 	}
