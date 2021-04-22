88783d83270ce45fcbd6b5941d1c9b773b3fb99a
Julia K <kurashvili@mail.hs-ulm.de>
2021-04-22 17:23:38 +0200

    Updated TestDataUpload.java
    updated to test the changes made to DataUpload.java

--- src/main/java/upload/UploadData.java
+++ src/main/java/upload/UploadData.java
@@ -1,47 +1,62 @@
 package upload;
 
-import java.io.BufferedReader;
+
 import java.util.ArrayList;
 
 import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 
+import model.ObservationModel;
 import server.Server;
 
 /************************************************************
  * @Author: Keno Maerz, Jonas Schick, Julia Kurashvili
  * 
- * UploadData-Class
+ * UploadData-Class (to be cleaned)
  * **********************************************************/
 
 public class UploadData {
 
-	public boolean filter(Server server, ArrayList<String> list) {
+	public String filter(Server server, ArrayList<String> list) {
 		int length = list.size();
 		AdministrativeGender gender = null;
 		String nachname = null;
 		String vorname = null;
+		String catwert = null;
+		double numwert = 0;
+		
+		ObservationModel observation;
 		for(int i = 0; i < length; i+=2) {
 			String varname = list.get(i);
 			String varwert = list.get(i+1);
 			if(varname.contentEquals("name")) {
 				vorname = varwert;
 			} else if(varname.contentEquals("familyname")) {
-				nachname = varname;
+				nachname = varwert;
 			} else if(varname.contentEquals("gender")) {
 				gender = Enumerations.AdministrativeGender.fromCode(varwert);
 			}
-			
-			/*if((var.contentEquals("bilirubin_concentration")) | (var.contentEquals("hepatitis_b"))) {
-				String observationCode = var;
-				
-				
-			}*/
 		}
 		if((gender == null) | (nachname == null) | (vorname == null)) {
-			return false;
+			return null;
 		} 
-		String patientID = server.createPatient(vorname, nachname, gender);
-		return true;
+		String patientID = server.createPatient(nachname, vorname, gender);
+		
+		for(int i = 0; i < length; i+=2) {
+			String varname = list.get(i);
+			String varwert = list.get(i+1);
+			if(varname.contentEquals("bilirubin_concentration")) {
+				numwert = Double.parseDouble(varwert);
+				observation = new ObservationModel("http://sfb125.de/ontology/ihCCApplicationOntology/", "bilirubin_concentration", numwert, "mg/dl");
+				server.createNumericalObservation(observation, patientID);	
+				
+			} //else if(varname.contentEquals("hepatitis_b")) {
+//				numwert = Double.parseDouble(varwert);
+//				observation = new ObservationModel("http://sfb125.de/ontology/ihCCApplicationOntology/chronic_hepatitis_b_observation", "hepatitis_b", numwert, "y");
+//				server.createNumericalObservation(observation, patientID);
+//				
+//			}
+		}
+		return patientID;
 	}
 }
