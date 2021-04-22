664bdef7757e080f380ed45685212ef82a8eb6c4
Lisamaria <leble@mail.hs-ulm.de>
2021-04-22 18:15:00 +0200

    Read server-data
    getting data from the "RecievingData" class and replace empty indexes with "null"

--- src/main/java/Data_Formatting/ReadData.java
+++ src/main/java/Data_Formatting/ReadData.java
@@ -1,23 +1,37 @@
 package Data_Formatting;
 
+//import redcapx.RecievingData;
 import java.util.*;
+
 import java.io.IOException;
 import java.nio.*;
 import java.nio.file.*;
 
 public class ReadData {
 	
-	public void split() throws IOException {
+	public void split() throws IOException{
 		
-		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\lisam\\Documents\\Studium\\Projektarbeit\\Gruppe1\\Daten\\DATA_2021-03-31_2145.csv"));
-		String liste="";
-		StringBuffer sb = new StringBuffer(liste);	
+		// getting data from the RecievingData class;
+		redcapx.RecievingData testen = new redcapx.RecievingData();	
+		String rohdaten = testen.getData();
 		
-		for(int i = 0; i < lines.size(); i++) {
-			sb.append(lines.get(i)+ ",");
+		//List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\lisam\\Documents\\Studium\\Projektarbeit\\Gruppe1\\Daten\\DATA_2021-03-31_2145.csv"));
+		//	String liste="";
+		//	StringBuffer sb = new StringBuffer(liste);	
+		
+		//for(int i = 0; i < lines.size(); i++) {
+		//	sb.append(lines.get(i)+ ",");
+		//}
+		//liste = sb.toString();
+		
+		List<String> data = new ArrayList(Arrays.asList(rohdaten.split(",")));
+		
+		for(int i=0; i<data.size(); i++) {
+			if(data.get(i).isEmpty()) {
+				data.set(i, "null");
+			}
 		}
-		liste = sb.toString();	
-		List<String> data = new ArrayList(Arrays.asList(liste.split(",")));
+		System.out.println(data);	
 	}
 	
 	public static void main(String[] args) throws IOException {
