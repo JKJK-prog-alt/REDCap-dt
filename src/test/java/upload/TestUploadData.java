f329efe29b46a83a185673a52a426128c4060b5e
Julia K <kurashvili@mail.hs-ulm.de>
2021-04-22 15:15:10 +0200

    Added TestUploadData.java
    added to simulate how UploadData might work like

---
+++ src/test/java/upload/TestUploadData.java
@@ -0,0 +1,63 @@
+package upload;
+
+/************************************************************
+ * @Author: Jonas Schick, Julia Kurashvili
+ * 
+ * Test Class for UploadData
+ * **********************************************************/
+
+import model.ObservationModel;
+import server.Server;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import org.hl7.fhir.r4.model.Enumerations;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestUploadData {
+	private static Server server;
+	private static UploadData upload;
+	
+	@BeforeClass
+    public static void setUp() {
+        server = new Server();
+        server.testConnection();
+        upload = new UploadData();
+    }
+	
+	@Test
+	public void testFilter() {
+		ArrayList<String> allPatients = server.getPatients();
+		
+		ArrayList<String> daten = new ArrayList<String>();
+		daten.add("bilirubin_concentration");
+		daten.add("33");
+		daten.add("form_1_complete");
+		daten.add("2");
+		daten.add("gender");
+		daten.add("male");
+		daten.add("hepatitis_b");
+		daten.add("1");
+		daten.add("record_id");
+		daten.add("1");
+		daten.add("familyname");
+		daten.add("Dolittle");
+		daten.add("name");
+		daten.add("Jonas");
+		
+		
+		Boolean succsses = upload.filter(daten);
+		
+		int numberAllPatients = allPatients.size();
+		 ArrayList<String> allPatientsAdded = server.getPatients();
+		 //ArrayList<ObservationModel> observations = server.getObservationsOfPatient(patientID);
+		assertEquals("Anzahl an Patienten nicht gleich Arraygroesse",numberAllPatients + 1, allPatientsAdded.size());
+		assertEquals(true, succsses);
+		
+		
+		
+	}
+	
+	
+
+}
