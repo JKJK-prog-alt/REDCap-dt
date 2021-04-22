3cff68318725a7fcbdec689901f8123d23dc3e86
Nick <Noehme@mail.hs-ulm.de>
2021-04-22 18:09:15 +0200

    JSONConverter
    Changes on Code, JSON Format reading Problem must still be solved

--- src/main/java/Data_Formatting/JSONConverter.java
+++ src/main/java/Data_Formatting/JSONConverter.java
@@ -1,22 +1,53 @@
 package Data_Formatting;
 
+import java.util.*;
+
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.*;
+import java.nio.file.*;
+
 public class JSONConverter {
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	public static void main(String[] args) {
+	public void convert() throws IOException {
 		
-
-	}
-
+		
+		
+		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Kalli\\Desktop\\new.JSON"));
+        String response= lines.get(0);
+      
+		System.out.println(response);
+	    JSONObject res=new JSONObject(response);
+	    JSONArray array = res.getJSONArray("profitarray");
+	    ArrayList<JSONArray> profitList = new ArrayList<>();
+	    ArrayList<ArrayList<Integer>> finalList = new ArrayList<>();
+	    ArrayList<Integer> list1 = new ArrayList<>();
+	    
+	    for (int i = 0; i < array.length(); i++){
+	        profitList.add(array.getJSONArray(i));
+	    }
+	    
+	    for (int i = 0; i < profitList.size(); i++){
+	    	
+	        JSONArray array1 = profitList.get(i);
+	        list1 = new ArrayList<>();
+	        
+	        for (int j = 0; j < array1.length(); j++){
+	            list1.add(array1.getInt(j));
+	        }	       
+	        finalList.add(list1);
+	        System.out.println(finalList);
+	    }
+	}	
+	public static void main(String[] args) throws IOException {
+		
+	
+			JSONConverter conv = new JSONConverter();
+			conv.convert();
+	}  
 }
+
+	
+
