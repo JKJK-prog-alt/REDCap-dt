91420a97c9599d5d5ef248d396e8ab487c0d01fa
Julia K <kurashvili@mail.hs-ulm.de>
2021-04-28 16:44:39 +0200

    Changed DataUpload.java
    changed UploadData to incorporate ObsCreator class

--- src/main/java/upload/UploadData.java
+++ src/main/java/upload/UploadData.java
@@ -8,6 +8,7 @@ import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 
 import model.ObservationModel;
 import server.Server;
+import upload.ObsCreator;
 
 /************************************************************
  * @Author: Keno Maerz, Jonas Schick, Julia Kurashvili
@@ -24,7 +25,7 @@ public class UploadData {
 		String vorname = null;
 		String catwert = null;
 		double numwert = 0;
-		
+
 		ObservationModel observation;
 		for(int i = 0; i < length; i+=2) {
 			String varname = list.get(i);
@@ -41,21 +42,15 @@ public class UploadData {
 			return null;
 		} 
 		String patientID = server.createPatient(nachname, vorname, gender);
-		
+
 		for(int i = 0; i < length; i+=2) {
 			String varname = list.get(i);
 			String varwert = list.get(i+1);
-			if(varname.contentEquals("bilirubin_concentration")) {
-				numwert = Double.parseDouble(varwert);
-				observation = new ObservationModel("http://sfb125.de/ontology/ihCCApplicationOntology/", "bilirubin_concentration", numwert, "mg/dl");
+
+			observation = ObsCreator.createObservationModel(varname,varwert);
+
+			if(observation != null)
 				server.createNumericalObservation(observation, patientID);	
-				
-			} //else if(varname.contentEquals("hepatitis_b")) {
-//				numwert = Double.parseDouble(varwert);
-//				observation = new ObservationModel("http://sfb125.de/ontology/ihCCApplicationOntology/chronic_hepatitis_b_observation", "hepatitis_b", numwert, "y");
-//				server.createNumericalObservation(observation, patientID);
-//				
-//			}
 		}
 		return patientID;
 	}
