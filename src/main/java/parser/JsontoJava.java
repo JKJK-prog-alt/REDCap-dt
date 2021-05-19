6ee6fdf2e3fae40efb110aab604bf7f50db89fb2
Lisamaria <leble@mail.hs-ulm.de>
2021-05-19 15:20:30 +0200

    Finalizing parsing of the RedCap data into ArrayList
    created 2 ArrayLists of the class AbstractObservationModel and filled them with the relevant info & Code cleaning // Author: Niklaas-B. Oehme & Lisamaria Eble

--- src/main/java/parser/JsontoJava.java
+++ src/main/java/parser/JsontoJava.java
@@ -15,6 +15,8 @@ public class JsontoJava {
     ArrayList<Double> bili = new ArrayList<Double>();
     ArrayList<String> form_com = new ArrayList<String>();
     
+    ArrayList<AbstractObservationModel> absBoolObs = new ArrayList<AbstractObservationModel>();
+    ArrayList<AbstractObservationModel> absNumObs = new ArrayList<AbstractObservationModel>();
  //   ArrayList<String> obs = new ArrayList<String>();
 		
     public void converter() {
@@ -22,75 +24,58 @@ public class JsontoJava {
     	// getting JsonNode from RedCapServerUnirest;
     	redcapx.RedCapServerUnirest testen = new redcapx.RedCapServerUnirest();	
     	JsonNode rohdaten = testen.getData();
-		
-//    	List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\lisam\\Documents\\Studium\\Projektarbeit\\Gruppe1\\Daten\\test.json"));
-//		String rohdaten= lines.get(0);   	
-//      System.out.println(rohdaten);
-      
-    	// transform JsonNode separated into ArrayLists
+
+    	// transform JsonNode separated into different ArrayLists
         for (int i = 0; i < rohdaten.getArray().length(); i++) {
         	JSONObject jobj = rohdaten.getArray().getJSONObject(i);
-        	System.out.println(jobj);
-        	
+	
 //        	obs.add(jobj.getString("record_id"));
 //        	obs.add(jobj.getString("hepatitis_b"));       	
 //        	obs.add(jobj.getString("bilirubin_concentration"));
 //        	obs.add(jobj.getString("form_1_complete")); 
-        	
+     
+        	// adding the different elements to their respective ArrayLists
         	id.add(i, jobj.getString("record_id"));
         	gen.add(i, jobj.getString("gender"));
-        	hep.add(i, jobj.getBoolean("hepatitis_b"));       	
+        	
+        	// convert the String into understandable boolean format
+        	if(jobj.getString("hepatitis_b").contentEquals("1")) {
+        		hep.add(i, true);
+        	} else {
+        		hep.add(i, false);
+        	}
         	bili.add(i, jobj.getDouble("bilirubin_concentration"));
         	form_com.add(i, jobj.getString("form_1_complete"));       	
         }
-        
-//      System.out.println(id);
-//    	System.out.println(gen);
-//    	System.out.println(hep);
-//    	System.out.println(bili);
-//    	System.out.println(form_com);
-    	
-    	for (int i = 0; i < id.size(); i++) {
-    		Datamodel data = new Datamodel(id.get(i).toString(),gen.get(i).toString(),hep.get(i),bili.get(i).doubleValue(),form_com.get(i).toString());
-			System.out.println(data);
-			
-			BooleanObservationModel boolobs = new BooleanObservationModel(configuration.observationSystem_BILIRUBIN, configuration.observationCode_BILIRUBIN_CODE, hep.get(i));
-			System.out.println(boolobs);
-			
-			NumericalObservationModel numobs = new NumericalObservationModel(configuration.observationSystem_HEPATITIS_B, configuration.observationCode_HEPATITIS_B_CODE, bili.get(i).doubleValue(), configuration.UNIT);
-			System.out.println(numobs);
-			
-		}
-        
-// https://www.youtube.com/watch?v=ynO4_XtUdOg
-
     }
-    // transfer data to BooleanObservationModel
-    public void booleanObs () {
+    // fill object of BooleanObservationModel with the data & return an ArrayList of AbstractObservationModel
+    public ArrayList<AbstractObservationModel> booleanObs () {
     	
     	for (int i = 0; i < hep.size(); i++) {
-    	
 			BooleanObservationModel boolobs = new BooleanObservationModel(configuration.observationSystem_BILIRUBIN, configuration.observationCode_BILIRUBIN_CODE, hep.get(i));
-			System.out.println(boolobs);
+			absBoolObs.add(i, boolobs);
 		}
+    	return absBoolObs;
     }
-    // transfer data to NumericalObservationModel
-    public void numericalObs() {
+    // fill object of NumericalObservationModel with the data & return an ArrayList of AbstractObservationModel
+    public ArrayList<AbstractObservationModel> numericalObs() {
     	
     	for (int i = 0; i < bili.size(); i++) {
-    		
 			NumericalObservationModel numobs = new NumericalObservationModel(configuration.observationSystem_HEPATITIS_B, configuration.observationCode_HEPATITIS_B_CODE, bili.get(i).doubleValue(), configuration.UNIT);
-			System.out.println(numobs);
+			absNumObs.add(i, numobs);
 		}
+    	return absNumObs;
     }
-    // transfer data to Datamodel
-    public void allObs() {
-    	
-    	for (int i = 0; i < id.size(); i++) {
-    		Datamodel data = new Datamodel(id.get(i).toString(),gen.get(i).toString(),hep.get(i),bili.get(i).doubleValue(),form_com.get(i).toString());
-			System.out.println(data);
-    	}
-    }
+//    // transfer all data to Datamodel
+//    public void allObs() {
+//    	
+//    	for (int i = 0; i < id.size(); i++) {
+//    		
+//    		Datamodel data = new Datamodel(id.get(i).toString(),gen.get(i).toString(),hep.get(i),bili.get(i).doubleValue(),form_com.get(i).toString());
+//			//System.out.println(data);
+//			
+//    	}
+//    }
     
     public static void main(String[] args) {
 
@@ -98,7 +83,6 @@ public class JsontoJava {
 		conv.converter();
 		conv.numericalObs();
 		conv.booleanObs();
-		conv.allObs();
-           
+		//conv.allObs();     
     }
 }
