c2abb81d28638eb4fe19e9c27ec9cd26a06eb3fc
Jonas Schick <joschick@mail.hs-ulm.de>
2021-05-20 12:48:29 +0200

    Connected UploadData.java to the Parser
    Now the parsed data can be uploaded to the server >> Authors: Julia and Jonas

--- src/main/java/upload/UploadData.java
+++ src/main/java/upload/UploadData.java
@@ -4,6 +4,7 @@ package upload;
 import java.util.ArrayList;
 import org.hl7.fhir.r4.model.Enumerations;
 import model.*;
+import parser.JsontoJava;
 import server.Server;
 
 /************************************************************
@@ -17,13 +18,39 @@ public class UploadData {
 	private String family = "Bobbington";
 	private String name = "Bob";
 	
+	public String transferData(Server server) {
+		JsontoJava conv = new JsontoJava();
+        conv.converter();
+        ArrayList<AbstractObservationModel> num = conv.numericalObs();
+        ArrayList<AbstractObservationModel> bool = conv.booleanObs();
+        ArrayList<String> gen = conv.getGen();
+        ArrayList<ArrayList<AbstractObservationModel>> patientList = new ArrayList<ArrayList<AbstractObservationModel>>();
+        int length = gen.size();
+        
+        for(int i = 0; i < length; i++) {
+        	ArrayList<AbstractObservationModel> data = new ArrayList<AbstractObservationModel>();
+        	data.add(num.get(i));
+        	data.add(bool.get(i));
+        	patientList.add(data);
+        }
+        
+		return upload(server, patientList, gen);
+	}
+	
 	/* uploads observations for one patient
 	 * 
 	 * */
 	public String upload(Server server, ArrayList<AbstractObservationModel> data, String gender) {
 		int length = data.size();
+		Enumerations.AdministrativeGender enumGender;
 		
-		Enumerations.AdministrativeGender enumGender = Enumerations.AdministrativeGender.fromCode(gender);
+		try {
+			enumGender = Enumerations.AdministrativeGender.fromCode(gender);
+			
+		}catch(Exception e) {
+			System.out.println("Unkown gender, changed to other");
+			enumGender = Enumerations.AdministrativeGender.OTHER;
+		}
 		String patientID = server.createPatient(family, name, enumGender);
 		
 		for(int i = 0; i < length; i++) {
