1a5b78ffecd1f92cfe3bf551930fe5605a292cd1
Nick <Noehme@mail.hs-ulm.de>
2021-05-20 09:51:02 +0200

    Code Correction and Getter Addition
    Corrected the insertion from the configuration class and cleaned the code of unnecessary lines. Added / to the arguments in configuration class and corrected the Value that it matches with the Server Author : Lisamaria Eble and Niklaas-B. Oehme

--- src/main/java/parser/JsontoJava.java
+++ src/main/java/parser/JsontoJava.java
@@ -17,7 +17,6 @@ public class JsontoJava {
     
     ArrayList<AbstractObservationModel> absBoolObs = new ArrayList<AbstractObservationModel>();
     ArrayList<AbstractObservationModel> absNumObs = new ArrayList<AbstractObservationModel>();
- //   ArrayList<String> obs = new ArrayList<String>();
 		
     public void converter() {
     	
@@ -28,12 +27,7 @@ public class JsontoJava {
     	// transform JsonNode separated into different ArrayLists
         for (int i = 0; i < rohdaten.getArray().length(); i++) {
         	JSONObject jobj = rohdaten.getArray().getJSONObject(i);
-	
-//        	obs.add(jobj.getString("record_id"));
-//        	obs.add(jobj.getString("hepatitis_b"));       	
-//        	obs.add(jobj.getString("bilirubin_concentration"));
-//        	obs.add(jobj.getString("form_1_complete")); 
-     
+   
         	// adding the different elements to their respective ArrayLists
         	id.add(i, jobj.getString("record_id"));
         	gen.add(i, jobj.getString("gender"));
@@ -52,7 +46,7 @@ public class JsontoJava {
     public ArrayList<AbstractObservationModel> booleanObs () {
     	
     	for (int i = 0; i < hep.size(); i++) {
-			BooleanObservationModel boolobs = new BooleanObservationModel(configuration.observationSystem_BILIRUBIN, configuration.observationCode_BILIRUBIN_CODE, hep.get(i));
+			BooleanObservationModel boolobs = new BooleanObservationModel(configuration.observationSystem_HEPATITIS_B, configuration.observationCode_HEPATITIS_B_CODE, hep.get(i));
 			absBoolObs.add(i, boolobs);
 		}
     	return absBoolObs;
@@ -61,28 +55,22 @@ public class JsontoJava {
     public ArrayList<AbstractObservationModel> numericalObs() {
     	
     	for (int i = 0; i < bili.size(); i++) {
-			NumericalObservationModel numobs = new NumericalObservationModel(configuration.observationSystem_HEPATITIS_B, configuration.observationCode_HEPATITIS_B_CODE, bili.get(i).doubleValue(), configuration.UNIT);
+			NumericalObservationModel numobs = new NumericalObservationModel(configuration.observationSystem_BILIRUBIN, configuration.observationCode_BILIRUBIN_CODE, bili.get(i).doubleValue(), configuration.UNIT);
 			absNumObs.add(i, numobs);
 		}
     	return absNumObs;
     }
-//    // transfer all data to Datamodel
-//    public void allObs() {
-//    	
-//    	for (int i = 0; i < id.size(); i++) {
-//    		
-//    		Datamodel data = new Datamodel(id.get(i).toString(),gen.get(i).toString(),hep.get(i),bili.get(i).doubleValue(),form_com.get(i).toString());
-//			//System.out.println(data);
-//			
-//    	}
-//    }
-    
-    public static void main(String[] args) {
+    // Getter method for the gender Array
+    	public ArrayList<String> getGen() {
+    		return gen;
+    	}
 
-    	JsontoJava conv = new JsontoJava();
-		conv.converter();
-		conv.numericalObs();
-		conv.booleanObs();
-		//conv.allObs();     
-    }
+//    public static void main(String[] args) {
+//
+//    	JsontoJava conv = new JsontoJava();
+//		conv.converter();
+//		conv.numericalObs();
+//		conv.booleanObs();
+//		  
+//    }
 }
--- src/main/java/util/configuration.java
+++ src/main/java/util/configuration.java
@@ -2,9 +2,9 @@ package util;
 
 public class configuration {
 	
-	public static final String observationSystem_HEPATITIS_B = "http://sfb125.de/ontology/ihCCApplicationOntology";
-	public static final String observationSystem_BILIRUBIN = "http://sfb125.de/ontology/ihCCApplicationOntology";
+	public static final String observationSystem_HEPATITIS_B = "http://sfb125.de/ontology/ihCCApplicationOntology/";
+	public static final String observationSystem_BILIRUBIN = "http://sfb125.de/ontology/ihCCApplicationOntology/";
 	public static final String observationCode_HEPATITIS_B_CODE = "chronic_hepatitis_b_observation";
-	public static final String observationCode_BILIRUBIN_CODE = "bilirubin_direct";
-	public static final String UNIT = "mg/dL";
+	public static final String observationCode_BILIRUBIN_CODE = "bilirubin_concentration";
+	public static final String UNIT = "mg/dl";
 }
